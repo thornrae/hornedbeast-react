22c35da8260e0f6bff1d87fd488201106fedfe75
Taylor Thornton <thornrae@gmail.com>
2022-01-14 12:08:38 -0800

    modal showing selectedBeast

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
 import Main from "./Main";
 import Header from "./Header";
 import Footer from "./Footer";
+import React from "react";
+import allBeasts from "./data.json";
+import SelectedBeast from "./SelectedBeast.js";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Main />
-      <Footer />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allBeasts: allBeasts,
+      selectedBeasts: {},
+      displayTheModal: false,
+    };
+  }
+
+  beastModalDisplay = (title) => {
+    const showThisBeastModal = allBeasts.find((beast) => beast.title === title);
+    console.log(showThisBeastModal);
+    this.setState({
+      selectedBeasts: showThisBeastModal,
+      displayTheModal: true,
+    });
+  };
+
+  closeModal = () => {
+    this.setState({ displayTheModal: false });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Main
+          allBeasts={this.state.allBeasts}
+          beastModalDisplay={this.beastModalDisplay}
+        />
+        <SelectedBeast
+          selectedBeasts={this.state.selectedBeasts}
+          displayTheModal={this.state.displayTheModal}
+          closeTheModal={this.closeModal}
+        />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Main.js
+++ src/Main.js
@@ -1,26 +1,19 @@
 import React from "react";
-import data from "./data.json";
 import "bootstrap/dist/css/bootstrap.min.css";
 import HornedBeast from "./HornedBeast";
 import "./Main.css";
 
 class Main extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      allBeasts: data,
-    };
-  }
-
   render() {
     return (
       <div className="grid-please">
-        {this.state.allBeasts.map((beast) => (
+        {this.props.allBeasts.map((beast) => (
           <HornedBeast
             title={beast.title}
             img={beast.image_url}
             description={beast.description}
             key={beast.title}
+            beastModalDisplay={this.props.beastModalDisplay}
           />
         ))}
       </div>
